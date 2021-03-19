f013daf07962213ac260cdd4dce02c558bd85e45
Alena1911 <alena-236@yandex.ru>
2021-03-19 18:51:43 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(225, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Каталог изображений";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(22, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Артикул";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(369, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Каталог загрузки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(100, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Загрузить из папки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(222, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Загрузка из папки (для новинок)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 136);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Поиск изображений товаров";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string path1 = System.IO.File.ReadAllText(@"path.txt", Encoding.Default);
+        string path2 = System.IO.File.ReadAllText(@"path2.txt", Encoding.Default);
+        int count = 0;
+        string copyerrors = "";
+
+        void AddIndex_Copy(string[] paths, bool chekmin = false)
+        {
+            string temp;
+            for (int i=0; i < paths.Length; i++)
+            {
+                try
+                {
+                    if (paths[i].IndexOf('_') < 0)
+                    {
+                        System.IO.FileInfo fileinf = new System.IO.FileInfo(paths[i]);
+                        if (fileinf.Length < 500000 && chekmin) throw new Exception("Файл меньше 500 Кб");
+                        if (fileinf.Length > 6000000 && (fileinf.Extension == ".jpg" || fileinf.Extension == ".jpeg"))
+                        {
+                            deliteEXIF(paths[i], path2 + System.IO.Path.GetFileNameWithoutExtension(paths[i]) + @"_IB01" + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                            fileinf = new FileInfo(path2 + System.IO.Path.GetFileNameWithoutExtension(paths[i]) + @"_IB01" + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                            if (fileinf.Length > 6000000)
+                            {
+                                File.Delete(path2 + System.IO.Path.GetFileNameWithoutExtension(paths[i]) + @"_IB01" + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                throw new Exception("Файл больше 6 Мб");
+                            }
+                            count++;
+                        }
+                        else if (fileinf.Length > 6000000)
+                        {
+                            throw new Exception("Файл больше 6 Мб");
+                        }
+                        else
+                        {
+                            File.Copy(paths[i], path2 + System.IO.Path.GetFileNameWithoutExtension(paths[i]) + @"_IB01" + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')), true);
+                            count++;
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    copyerrors += System.IO.Path.GetFileName(paths[i]) + " - " + ex.Message + '\n';
+                }
+                if (paths[i].IndexOf('_') >= 0)
+                {
+                    temp = Path.GetFileNameWithoutExtension(paths[i]);
+                    string index = temp.Substring(temp.IndexOf('_') + 1);
+                    if (index.Length == 4) index = index.Substring(2, 2);
+                    int Indx = Convert.ToInt32(index);
+                    try
+                    {
+                        FileInfo fileinf = new FileInfo(paths[i]);
+                        if (fileinf.Length < 500000 && chekmin) throw new Exception("Файл меньше 500 Кб");
+                        if (fileinf.Length > 6000000 && (fileinf.Extension == ".jpg" || fileinf.Extension == ".jpeg"))
+                        {
+                            if (chekmin)
+                            {
+                                if (Indx >= 10)
+                                {
+                                    deliteEXIF(paths[i], path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IB" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                    fileinf = new FileInfo(path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IB" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                    if (fileinf.Length > 6000000)
+                                    {
+                                        File.Delete(path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IB" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                        throw new Exception("Файл больше 6 Мб");
+                                    }
+                                    count++;
+                                }
+                                else if (Indx < 10)
+                                {
+                                    deliteEXIF(paths[i], path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IB" + @"0" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                    fileinf = new FileInfo(path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IB" + @"0" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                    if (fileinf.Length > 6000000)
+                                    {
+                                        File.Delete(path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IB" + @"0" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                        throw new Exception("Файл больше 6 Мб");
+                                    }
+                                    count++;
+                                }
+                            }
+                            else
+                            {
+                                if (Indx >= 10)
+                                {
+                                    deliteEXIF(paths[i], path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IN" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                    fileinf = new FileInfo(path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IN" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                    if (fileinf.Length > 6000000)
+                                    {
+                                        File.Delete(path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IN" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                        throw new Exception("Файл больше 6 Мб");
+                                    }
+                                    count++;
+                                }
+                                else if (Indx < 10)
+                                {
+                                    deliteEXIF(paths[i], path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IN" + @"0" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                    fileinf = new FileInfo(path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IN" + @"0" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                    if (fileinf.Length > 6000000)
+                                    {
+                                        File.Delete(path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IN" + @"0" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')));
+                                        throw new Exception("Файл больше 6 Мб");
+                                    }
+                                    count++;
+                                }
+                            }
+                        }
+                        else if (fileinf.Length > 6000000) throw new Exception("Файл больше 6 Мб");
+                        else if (chekmin)
+                        {
+                            if (Indx >= 10)
+                            {
+
+                                File.Copy(paths[i], path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IB" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')), true);
+                                count++;
+                            }
+                            else if (Indx < 10)
+                            {
+                                File.Copy(paths[i], path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IB" + @"0" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')), true);
+                                count++;
+                            }
+                        }
+                        else
+                        {
+                            if (Indx >= 10)
+                            {
+
+                                File.Copy(paths[i], path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IN" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')), true);
+                                count++;
+                            }
+                            else if (Indx < 10)
+                            {
+                                File.Copy(paths[i], path2 + temp.Substring(0, temp.IndexOf('_')) + @"_IN" + @"0" + Convert.ToString(Indx) + System.IO.Path.GetFileName(paths[i]).Substring(System.IO.Path.GetFileName(paths[i]).IndexOf('.')), true);
+                                count++;
+                            }
+                        }
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        string Mes = ex.Message;
+                        copyerrors += System.IO.Path.GetFileName(paths[i]) + " - " + Mes + '\n';
+                    }
+                }
+            }
+        }
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void deliteEXIF(string inPic, string outPic)
+        {
+            Bitmap bmp = new Bitmap(inPic);
+
+            foreach (PropertyItem item in bmp.PropertyItems)
+            {
+                if (item.Id == 0x0112 || item.Id == 0x0132)
+                    continue;
+
+                PropertyItem modItem = item;
+                modItem.Value = new byte[] { 0 };
+
+                bmp.SetPropertyItem(modItem);
+            }
+            bmp.Save(outPic);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (textBox1.Text.Length == 0) throw new Exception("Пустой запрос.");
+                if (Directory.Exists(path1 + textBox1.Text))
+                    System.Diagnostics.Process.Start("explorer", path1 + textBox1.Text);
+                else throw new Exception("Артикул не найден");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка! {ex.Message}");
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            path1 = path1.Replace("\t", @"");
+            path1 = path1.Replace("\r", @"");
+            path1 = path1.Replace("\n", @"");
+            path2 = path2.Replace("\t", @"");
+            path2 = path2.Replace("\r", @"");
+            path2 = path2.Replace("\n", @"");
+            if (path1[path1.Length - 1] != '\\') path1 += @"\";
+            if (path2[path2.Length - 1] != '\\') path2 += @"\";
+
+            label1.Text = path1;
+            if (!Directory.Exists(path1))
+            {
+                MessageBox.Show("Предупреждение! Директория с изображениями не найдена.");
+            }
+            if (!Directory.Exists(path2))
+            {
+                MessageBox.Show("Предупреждение! Каталог загрузки не найден.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer", path2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            try
+            {
+                //folderBrowserDialog1.ShowDialog();
+                //string DownloadPath = path1;
+                //string[] files_jpg = Directory.GetFiles(DownloadPath, "*.jpg", SearchOption.AllDirectories);
+                //string[] files_png = Directory.GetFiles(DownloadPath, "*.png", SearchOption.AllDirectories);
+                //string[] files_jpeg = Directory.GetFiles(DownloadPath, "*.jpeg", SearchOption.AllDirectories);
+                //if (files_jpg.Length == 0 && files_png.Length == 0) throw new Exception("Не найдено ни одного изображения");
+                //AddIndex_Copy(files_jpg);
+                //AddIndex_Copy(files_png);
+                //AddIndex_Copy(files_jpeg);
+
+                openFileDialog1.Multiselect = true;
+                openFileDialog1.FileName = "";
+                openFileDialog1.Filter = "Изображения|*.jpg;*.jpeg;*.png";
+                if (Directory.Exists(path1))
+                    openFileDialog1.InitialDirectory = path1;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string[] filenames = openFileDialog1.FileNames;
+                    AddIndex_Copy(filenames, true);
+                }
+
+
+                if (copyerrors.Length > 0)
+                {
+                    MessageBox.Show(copyerrors, "Ошибка при копировании файлов:");
+                    copyerrors = "";
+                }
+                if (count==1)
+                    MessageBox.Show($"Загружен 1 файл");
+                else if (count > 1 && count < 5)
+                    MessageBox.Show($"Загружено {count} файла");
+                else
+                    MessageBox.Show($"Загружено {count} файлов");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Ошибка! {ex.Message}");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            try
+            {
+                //folderBrowserDialog1.ShowDialog();
+                //string DownloadPath = folderBrowserDialog1.SelectedPath;
+                //string[] files_jpg = Directory.GetFiles(DownloadPath, "*.jpg", SearchOption.AllDirectories);
+                //string[] files_png = Directory.GetFiles(DownloadPath, "*.png", SearchOption.AllDirectories);
+                //string[] files_jpeg = Directory.GetFiles(DownloadPath, "*.jpeg", SearchOption.AllDirectories);
+                //if (files_jpg.Length == 0 && files_png.Length == 0) throw new Exception("Не найдено ни одного изображения");
+                //AddIndex_Copy_New(files_jpg);
+                //AddIndex_Copy_New(files_png);
+                //AddIndex_Copy_New(files_jpeg);
+
+                openFileDialog1.Multiselect = true;
+                openFileDialog1.Filter = "Изображения|*.jpg;*.jpeg;*.png";
+                openFileDialog1.FileName = "";
+                if (Directory.Exists(path1))
+                    openFileDialog1.InitialDirectory = path1;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string[] filenames = openFileDialog1.FileNames;
+                    AddIndex_Copy(filenames);
+                }
+
+                if (copyerrors.Length > 0)
+                {
+                    MessageBox.Show(copyerrors, "Ошибка при копировании файлов:");
+                    copyerrors="";
+                }
+                if (count == 1)
+                    MessageBox.Show($"Загружен 1 файл");
+                else if (count > 1 && count < 5)
+                    MessageBox.Show($"Загружено {count} файла");
+                else
+                    MessageBox.Show($"Загружено {count} файлов");
+                count = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка! {ex.Message}");
+            }
+        }
+    }
+}
